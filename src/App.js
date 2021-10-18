6d2a45a071f40dbd7bbadff98b85ddd82b2abeba
Arno De Pachter <arno@zeelandmultimedia.nl>
2021-10-18 10:35:36 +0200

    fixed auth0

--- src/App.js
+++ src/App.js
@@ -1,68 +1,51 @@
 import React from 'react';
-import './App.css';
-import ListShipments from './Shipments';
+import { Router, Route, Switch } from 'react-router-dom';
+import { Auth0Provider, withAuthenticationRequired } from '@auth0/auth0-react';
+import { createBrowserHistory } from 'history';
+import Profile from './components/Profile';
+
 import Home from './Home';
-import { Route, Switch } from 'react-router-dom';
-import DeleteShip from './DeleteShip';
 import IncomingOrders from './IncomingOrders';
+import ListShipments from './Shipments';
+import DeleteShip from './DeleteShip';
 import NewOrder from './NewOrder';
 import InkoPrint from './inkoprint';
-import Login from './Login';
-import { useAuth0 } from '@auth0/auth0-react';
-import Spinner from 'react-bootstrap/Spinner';
 
+export const history = createBrowserHistory();
 
+const ProtectedRoute = ({ component, ...args }) => (
+  <Route component={withAuthenticationRequired(component)} {...args} />
+);
+
+const onRedirectCallback = (appState) => {
+  // Use the router's history module to replace the url
+  history.replace(appState?.returnTo || window.location.pathname);
+};
 function App() {
-  const {
-    isLoading,
-    isAuthenticated,
-    error,
-    user,
-    loginWithRedirect,
-    logout,
-  } = useAuth0();
 
-  if (isLoading) {
-    return (
-    <Spinner animation="border" role="status">
-      <span className="visually-hidden">Loading...</span>
-    </Spinner>)
-  ;
-  }
-  if (error) {
-    return <div>Oops... {error.message}</div>;
-  }
-
-  if (isAuthenticated) {
-    return (
-    <>
-    <main>
-      <Switch>
-        
-        <Route path="/incoming" component={IncomingOrders} />
-        <Route path="/shipments" component={ListShipments} />
-        <Route path="/delete" component={DeleteShip} />
-        <Route path="/addnew" component={NewOrder} />
-        <Route path="/inkoprint" component={InkoPrint} />
-        <Route path="/login" component={Login} />
-      </Switch>
-    </main>
-    <main>
-    <Switch>
-        <Route path="/" component={Home} exact />
-    </Switch>
-    </main>
-    </>
+  return (
+    <Auth0Provider
+      domain="dev-bzuojegv.us.auth0.com"
+      clientId="BzDcUWqQgLKULNMFDqZsJjr9HpcYwFpE"
+      redirectUri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {/* Don't forget to add the history to your router */}
+      <Router history={history}>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          
+          <ProtectedRoute path="/incoming" component={IncomingOrders} />
+          <ProtectedRoute path="/shipments" component={ListShipments} />
+          <ProtectedRoute path="/delete" component={DeleteShip} />
+          <ProtectedRoute path="/addnew" component={NewOrder} />
+          <ProtectedRoute path="/inkoprint" component={InkoPrint} />
+          
+          <ProtectedRoute path="/profile" component={Profile} />
+        </Switch>
+      </Router>
+    </Auth0Provider>
   );
-} else if (!isAuthenticated) {
-  return(
-  <main>
-    <Switch>
-        <Route path="/" component={Home} exact />
-    </Switch>
-    </main>
-  )
-}
 }
 
 
